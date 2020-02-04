b348342e08ab60573cdc796516c96ee9ac5f42d0
cwinters8 <clwinters8@gmail.com>
2020-02-03 21:29:25 -0800

    working subscribe function

--- functions/subscribe/subscribe.js
+++ functions/subscribe/subscribe.js
@@ -1,7 +1,42 @@
-const axios = require('axios');
+const axios = require('axios').default;
 
 // Serverless function that subscribes an email address to the MailChimp mailing list
 exports.handler = (event, context, callback) => {
+  const mailchimp = 'https://us4.api.mailchimp.com/3.0';
   const apiKey = process.env.APIKEY;
-  console.log(apiKey);
+  const listId = process.env.LISTID;
+  const path = `${mailchimp}/lists/${listId}/members`;
+  const eventBody = JSON.parse(event.body);
+  const email = eventBody.email;
+  const firstName = eventBody.firstName;
+  const lastName = eventBody.lastName;
+  if (!email) {
+    // send an error if no email is found
+    callback({error: 'Email not found'});
+    throw 'Email not found';
+  }
+  // build an object to send with the post request
+  const data = {
+    email_address: email,
+    status: 'subscribed',
+    merge_fields: {
+      "FNAME": firstName,
+      "LNAME": lastName
+    }
+  };
+  // send post request to api
+  axios.post(path, JSON.stringify(data), {
+    auth: {
+      username: 'none',
+      password: apiKey
+    }
+  }).then(res => {
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify({data: res.data})
+    });
+  }).catch(err => {
+    callback(err);
+    throw err;
+  });
 }
