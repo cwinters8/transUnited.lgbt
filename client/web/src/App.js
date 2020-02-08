9cad1f32d637c959bb54103a25422299662d1560
cwinters8 <clwinters8@gmail.com>
2020-02-07 20:14:09 -0800

    add Alpha to title

--- client/web/src/App.js
+++ client/web/src/App.js
@@ -11,7 +11,7 @@ const App = () => {
   return (
     <div className="App">
       <Navbar expand="md">
-        <NavbarBrand href="/"><strong>transUnited</strong></NavbarBrand>
+        <NavbarBrand href="/"><strong>transUnited Alpha</strong></NavbarBrand>
       </Navbar>
       <div className="main">
         <p>After some frustration with existing social media platforms, we are building a new social media app for transgender people and our allies. Our goal is to create a trans friendly space where we can safely share our lives and educate each other.</p>
